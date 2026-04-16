85c45979ed0c18f0a0ca2f3516d9cd4d14ec1b01
Tasnim813 <tasnim3j@gmail.com>
2026-04-16 18:11:30 +0600

    load title

--- post.js
+++ post.js
@@ -9,8 +9,15 @@ const loadPost=()=>{
 }
 
 const displayPost=(posts)=>{
+    const postContainer=document.getElementById('post-container')
+    // console.log(postContainer)
     posts.forEach((post)=>{
-        console.log(post)
+        // console.log(post.title)
+        const li=document.createElement('li')
+        li.innerText=post.title;
+        console.log(li)
+
+        postContainer.appendChild(li)
     })
     
 }
