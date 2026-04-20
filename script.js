7cd17e5439353f5e9bef605417a79907f58f6f8e
Tasnim813 <tasnim3j@gmail.com>
2026-04-20 19:55:03 +0600

    remove

--- script.js
+++ script.js
@@ -10,12 +10,3 @@ const loadData=()=>{
 
 
 
-const loadData=()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(res=>res.json())
-.then(data=>console.log(data))
-
-}
-
-
-console.log(loadData)
